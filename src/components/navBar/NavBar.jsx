ad2568ea56178b6a4d1bd736066a059d8f764215
Artem <voylenko.2002@gmail.com>
2022-11-20 15:23:39 +0100

    Minor fixes

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,158 +1,296 @@
-import { Component } from 'react';
-import { createRef } from 'react';
-import { request } from 'graphql-request';
-import { Link } from 'react-router-dom';
+import { Component } from "react";
+import { createRef } from "react";
+import { request } from "graphql-request";
+import { Link } from "react-router-dom";
 
-import GET_PRODUCTS from '../../graphql/getProducts';
-import DELETE_ITEM from '../../graphql/deleteItem';
+import GET_PRODUCTS from "../../graphql/getProducts";
+import DELETE_ITEM from "../../graphql/deleteItem";
 import GET_CART from "../../graphql/getCart";
 
-import './navBar.sass';
+import "./navBar.sass";
 
 class NavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.currencyRef = createRef();
-        this.cartRef = createRef();
-        this.state = {
-           productsFiltered: [],
-           currency: '$',
-           cartItems: null,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.currencyRef = createRef();
+    this.cartRef = createRef();
+    this.state = {
+      productsFiltered: [],
+      currency: "$",
+      cartItems: null,
+    };
+  }
 
-    componentDidMount() {
-        this.filterProducts();
-        this.setState({cartItems: this.props.cartItems});
-    }
+  componentDidMount() {
+    this.filterProducts();
+    this.setState({ cartItems: this.props.cartItems });
+  }
 
-    componentDidUpdate(prevState, prevProps) {
-        if (prevProps.cartItems !== this.props.cartItems) {
-            this.setState({cartItems: this.props.cartItems});
-        }
+  componentDidUpdate(prevState, prevProps) {
+    if (prevProps.cartItems !== this.props.cartItems) {
+      this.setState({ cartItems: this.props.cartItems });
     }
-    
-    onCurrencyModal = () => {
-        const curRef = this.currencyRef.current;
-        curRef.classList.toggle('show');
-    }
+  }
 
-    onCartModal = () => {
-        const curRef = this.cartRef.current;
-        curRef.classList.toggle('show');
-    }
+  onCurrencyModal = () => {
+    const curRef = this.currencyRef.current;
+    curRef.classList.toggle("show");
+  };
 
-    onChangeCurrency = (e) => {
-        if (e.target.id === 'usd') {
-            this.setState(({currency}) => ({currency: '$'}), () => this.props.setCurrency(this.state.currency))
-        }
-        if (e.target.id === 'eur') {
-            this.setState(({currency}) => ({currency: '€'}), () => this.props.setCurrency(this.state.currency))
-        }
-        if (e.target.id === 'pln') {
-            this.setState(({currency}) => ({currency: 'zł'}), () => this.props.setCurrency(this.state.currency))
-        }
-        if (e.target.id === 'uah') {
-            this.setState(({currency}) => ({currency: '₴'}), () => this.props.setCurrency(this.state.currency))
-        };
-        
-    }
+  onCartModal = () => {
+    const curRef = this.cartRef.current;
+    curRef.classList.toggle("show");
+  };
 
-    onFilterProducts = (e) => {
-        const category = e.target.id;
-        this.filterProducts(category);
+  onChangeCurrency = (e) => {
+    if (e.target.id === "usd") {
+      this.setState(
+        ({ currency }) => ({ currency: "$" }),
+        () => this.props.setCurrency(this.state.currency)
+      );
     }
-    
-    filterProducts = (categoryTitle = 'All') => {
-         request('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master', GET_PRODUCTS)
-        .then(categoryTitle !== 'All' ? 
-        data => this.setState({productsFiltered: data.products.filter(i => i.category[1].categoryTitle === categoryTitle)}, () => {this.props.filteredData(this.state.productsFiltered);}) : 
-        data => this.setState({productsFiltered: data.products.filter(i => i.category[0].categoryTitle)}, () => {this.props.filteredData(this.state.productsFiltered);}));
+    if (e.target.id === "eur") {
+      this.setState(
+        ({ currency }) => ({ currency: "€" }),
+        () => this.props.setCurrency(this.state.currency)
+      );
     }
+    if (e.target.id === "pln") {
+      this.setState(
+        ({ currency }) => ({ currency: "zł" }),
+        () => this.props.setCurrency(this.state.currency)
+      );
+    }
+    if (e.target.id === "uah") {
+      this.setState(
+        ({ currency }) => ({ currency: "₴" }),
+        () => this.props.setCurrency(this.state.currency)
+      );
+    }
+  };
 
-    onDeleteItem = (id) => {
-        request('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master', DELETE_ITEM(id))
-        .then(() => this.onGetCart())
-        .then(() => this.props.setCartItems(this.state.cartItems));
-    }
+  onFilterProducts = (e) => {
+    const category = e.target.id;
+    this.filterProducts(category);
+  };
 
-    onGetCart = () => {
-        request('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master', GET_CART)
-        .then(data => this.setState({cartItems: data.carts}));
-    }
+  filterProducts = (categoryTitle = "All") => {
+    request(
+      "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master",
+      GET_PRODUCTS
+    ).then(
+      categoryTitle !== "All"
+        ? (data) =>
+            this.setState(
+              {
+                productsFiltered: data.products.filter(
+                  (i) => i.category[1].categoryTitle === categoryTitle
+                ),
+              },
+              () => {
+                this.props.filteredData(this.state.productsFiltered);
+              }
+            )
+        : (data) =>
+            this.setState(
+              {
+                productsFiltered: data.products.filter(
+                  (i) => i.category[0].categoryTitle
+                ),
+              },
+              () => {
+                this.props.filteredData(this.state.productsFiltered);
+              }
+            )
+    );
+  };
 
-    render() {
-        const RenderItems = (arr) => { //  сейчас при получении данных они пушатся в стейт, я дуамю что мне это не надо т.к они удалятся оттуда при перезагрузке страницы, надо придумать как запихнуть данные из апишки в корзину
-            const { currency } = this.state;
-            const items = arr.map((item) => {
-              return (
-                <div className="miniCart__products_item" key={item.id}>
-                   <div className="miniCart__products-item-title">{item.title}</div>
-                   <div className="miniCart__products-item-price">
-                { currency === '$' ? `$ ${item.price}` : 
-                currency === '€' ? `€ ${(item.price * 0.97).toFixed(0)}` : 
-                currency === 'zł' ? `zł ${(item.price * 4.54).toFixed(0)}` :
-                currency === '₴' ? `₴ ${(item.price * 36.80).toFixed(0)}` : `$ ${item.price}` }
-                    </div>
-                <div className="miniCart__products-item-size">Size: {item.size}</div>
-                    <div className="miniCart__products-item-quantity">
-                        <div className="miniCart__products-item-quantity-text">Quantity: </div>
-                        <div className="miniCart__products-item-quantity-number">{item.quantity}</div>
-                        <img className="miniCart__products-item-trash" src="https://cdn-icons-png.flaticon.com/512/3096/3096673.png" onClick={() => this.onDeleteItem(item.id)} alt="trash" />
-                    </div>
-                <img className="miniCart__products-item-img" src={item.mainImage} alt={item.title} />  
-            <hr></hr>  
-                </div>
-              );
-            });
-            return <div className="miniCart__products">
-            {items}
-        </div>;
-          }
-          const render = !this.state.cartItems ? <div className="miniCart__empty">Cart is empty</div> : this.state.cartItems.length === 0 ? <div className="miniCart__empty">Cart is empty</div> : RenderItems(this.state.cartItems); 
+  onDeleteItem = (id) => {
+    request(
+      "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master",
+      DELETE_ITEM(id)
+    )
+      .then(() => this.onGetCart())
+      .then(() => this.props.setCartItems(this.state.cartItems));
+  };
+
+  onGetCart = () => {
+    request(
+      "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clagsd8q200sb01uk9785hs2l/master",
+      GET_CART
+    ).then((data) => this.setState({ cartItems: data.carts }));
+  };
+
+  render() {
+    const RenderItems = (arr) => {
+      //  сейчас при получении данных они пушатся в стейт, я дуамю что мне это не надо т.к они удалятся оттуда при перезагрузке страницы, надо придумать как запихнуть данные из апишки в корзину
+      const { currency } = this.state;
+      const items = arr.map((item) => {
         return (
-            <>
-            <nav className="navBar">
-        <div className="navBar__wrapper">
-                    <Link to="/">
-                    <img className="navBar__icon" src="https://cdn-icons-png.flaticon.com/128/2898/2898372.png" alt="shop-icon" />
-                    </Link>
-                    <Link className="navBar__types" to="/">
-                        <div className="navBar__types" >
-                            <div id="All" className="navBar__types-item" onClick={this.onFilterProducts}>All</div>
-                            <div id="Clothes" className="navBar__types-item" onClick={this.onFilterProducts}>Clothes</div>
-                            <div id="Tech" className="navBar__types-item" onClick={this.onFilterProducts}>Tech</div>
-                        </div>
-                    </Link>
-                    <div className="navBar__currency" onClick={this.onCurrencyModal}>
-                        <div className="navBar__currency-symbol">{this.state.currency}</div>
-                        <div className="navBar__currency-btn"><img style={{'width': 10}} src="https://cdn-icons-png.flaticon.com/128/32/32195.png" alt="dropdown" /></div>
-                        <ul ref={this.currencyRef} className="navBar__currency-content" >
-                            <li id="usd" className="navBar__currency-contentSymbol" onClick={this.onChangeCurrency}>$ USD</li>
-                            <li id="eur" className="navBar__currency-contentSymbol" onClick={this.onChangeCurrency}>€ EUR</li>
-                            <li id="pln" className="navBar__currency-contentSymbol" onClick={this.onChangeCurrency}>zł PLN</li>
-                            <li id="uah" className="navBar__currency-contentSymbol" onClick={this.onChangeCurrency}>₴ UAH</li>
-                        </ul>
-                    </div>
-                        <div className="navBar__cart" >
-                            <img className="navBar__cart-img" src="https://cdn-icons-png.flaticon.com/512/2838/2838895.png" alt="shop-cart" onClick={this.onCartModal} />
-                            {!this.state.cartItems ? null : this.state.cartItems.length === 0 ? null : <div className="navBar__cart-quantity">{this.state.cartItems.length}</div>}
-                                <div ref={this.cartRef} className="miniCart">
-                                    {render}
-                                        <Link to="/cart">
-                                            <button className="miniCart__button-cart" onClick={this.onCartModal}>GO TO CART</button>
-                                        </Link>
-                                        <Link to="/checkout">
-                                    <button className="miniCart__button-check" onClick={this.onCartModal}>CHECKOUT</button>  
-                                    </Link>          
-                                </div>
-                        </div>
-                    </div>
-                </nav>
-            </>
+          <div className="miniCart__products_item" key={item.id}>
+            <div className="miniCart__products-item-title">{item.title}</div>
+            <div className="miniCart__products-item-price">
+              {currency === "$"
+                ? `$ ${item.price}`
+                : currency === "€"
+                ? `€ ${(item.price * 0.97).toFixed(0)}`
+                : currency === "zł"
+                ? `zł ${(item.price * 4.54).toFixed(0)}`
+                : currency === "₴"
+                ? `₴ ${(item.price * 36.8).toFixed(0)}`
+                : `$ ${item.price}`}
+            </div>
+            <div className="miniCart__products-item-size">
+              Size: {item.size}
+            </div>
+            <div className="miniCart__products-item-quantity">
+              <div className="miniCart__products-item-quantity-text">
+                Quantity:{" "}
+              </div>
+              <div className="miniCart__products-item-quantity-number">
+                {item.quantity}
+              </div>
+              <img
+                className="miniCart__products-item-trash"
+                src="https://cdn-icons-png.flaticon.com/512/3096/3096673.png"
+                onClick={() => this.onDeleteItem(item.id)}
+                alt="trash"
+              />
+            </div>
+            <img
+              className="miniCart__products-item-img"
+              src={item.mainImage}
+              alt={item.title}
+            />
+            <hr></hr>
+          </div>
         );
-    }
+      });
+      return <div className="miniCart__products">{items}</div>;
+    };
+    const render = !this.state.cartItems ? (
+      <div className="miniCart__empty">Cart is empty</div>
+    ) : this.state.cartItems.length === 0 ? (
+      <div className="miniCart__empty">Cart is empty</div>
+    ) : (
+      RenderItems(this.state.cartItems)
+    );
+    return (
+      <>
+        <nav className="navBar">
+          <div className="navBar__wrapper">
+            <Link to="/">
+              <img
+                className="navBar__icon"
+                src="https://cdn-icons-png.flaticon.com/128/2898/2898372.png"
+                alt="shop-icon"
+              />
+            </Link>
+            <Link className="navBar__types" to="/">
+              <div className="navBar__types">
+                <div
+                  id="All"
+                  className="navBar__types-item"
+                  onClick={this.onFilterProducts}
+                >
+                  All
+                </div>
+                <div
+                  id="Clothes"
+                  className="navBar__types-item"
+                  onClick={this.onFilterProducts}
+                >
+                  Clothes
+                </div>
+                <div
+                  id="Tech"
+                  className="navBar__types-item"
+                  onClick={this.onFilterProducts}
+                >
+                  Tech
+                </div>
+              </div>
+            </Link>
+            <div className="navBar__currency" onClick={this.onCurrencyModal}>
+              <div className="navBar__currency-symbol">
+                {this.state.currency}
+              </div>
+              <div className="navBar__currency-btn">
+                <img
+                  style={{ width: 10 }}
+                  src="https://cdn-icons-png.flaticon.com/128/32/32195.png"
+                  alt="dropdown"
+                />
+              </div>
+              <ul ref={this.currencyRef} className="navBar__currency-content">
+                <li
+                  id="usd"
+                  className="navBar__currency-contentSymbol"
+                  onClick={this.onChangeCurrency}
+                >
+                  $ USD
+                </li>
+                <li
+                  id="eur"
+                  className="navBar__currency-contentSymbol"
+                  onClick={this.onChangeCurrency}
+                >
+                  € EUR
+                </li>
+                <li
+                  id="pln"
+                  className="navBar__currency-contentSymbol"
+                  onClick={this.onChangeCurrency}
+                >
+                  zł PLN
+                </li>
+                <li
+                  id="uah"
+                  className="navBar__currency-contentSymbol"
+                  onClick={this.onChangeCurrency}
+                >
+                  ₴ UAH
+                </li>
+              </ul>
+            </div>
+            <div className="navBar__cart">
+              <img
+                className="navBar__cart-img"
+                src="https://cdn-icons-png.flaticon.com/512/2838/2838895.png"
+                alt="shop-cart"
+                onClick={this.onCartModal}
+              />
+              {!this.state.cartItems ? null : this.state.cartItems.length ===
+                0 ? null : (
+                <div className="navBar__cart-quantity">
+                  {this.state.cartItems.length}
+                </div>
+              )}
+              <div ref={this.cartRef} className="miniCart">
+                {render}
+                <Link to="/cart">
+                  <button
+                    className="miniCart__button-cart"
+                    onClick={this.onCartModal}
+                  >
+                    GO TO CART
+                  </button>
+                </Link>
+                <Link to="/checkout">
+                  <button
+                    className="miniCart__button-check"
+                    onClick={this.onCartModal}
+                  >
+                    CHECKOUT
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
 
 export default NavBar;
-
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,44 +1,80 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
 
-import './productList.sass';
+import "./productList.sass";
 
 class ProductList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            itemPrice: 0,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemPrice: 0,
+    };
+  }
 
-    render() { 
-       const {productsFiltered} = this.props;
-        return (
-            <main className="productList">
-                {!productsFiltered ? null : productsFiltered.map((product) => product.quantity !==0 ? 
-                (<li className="productList__item" key={product.id} onClick={() => {this.props.onProductSelected(product.slug)}}>
-                <Link to={`/product/${product.slug}`} >
-                <img className="productList__item-img" src={product.mainImage.url} alt={product.title} />
+  render() {
+    const { productsFiltered } = this.props;
+    return (
+      <main className="productList">
+        {!productsFiltered
+          ? null
+          : productsFiltered.map((product) =>
+              product.quantity !== 0 ? (
+                <li
+                  className="productList__item"
+                  key={product.id}
+                  onClick={() => {
+                    this.props.onProductSelected(product.slug);
+                  }}
+                >
+                  <Link to={`/product/${product.slug}`}>
+                    <img
+                      className="productList__item-img"
+                      src={product.mainImage.url}
+                      alt={product.title}
+                    />
                     <div className="productList__item-descr">
-                        <div className="productList__item-title">{product.title}</div>
-                        <div className="productList__item-price">{
-                        this.props.currency === '$' ? `$ ${product.price}` : 
-                        this.props.currency === '€' ? `€ ${(product.price * 0.97).toFixed(0)}` : 
-                        this.props.currency === 'zł' ? `zł ${(product.price * 4.54).toFixed(0)}` :
-                        this.props.currency === '₴' ? `₴ ${(product.price * 36.80).toFixed(0)}` : `$ ${product.price}` }
-                        </div>
-                </div>
-                </Link>
-            </li>) : <li key={product.id} className="productList__item-missing" onClick={() => {this.props.onProductSelected(product.id)}}>
-                <img className="productList__item-img" src={product.mainImage.url} alt={product.title} />
-                    <div className="productList__item-descr">
-                        <div className="productList__item-title">{product.title}</div>
-                        <div className="productList__item-price">OUT OF STOCK</div>
-                </div>
-            </li>)}
-            </main>
-        );
-    }
+                      <div className="productList__item-title">
+                        {product.title}
+                      </div>
+                      <div className="productList__item-price">
+                        {this.props.currency === "$"
+                          ? `$ ${product.price}`
+                          : this.props.currency === "€"
+                          ? `€ ${(product.price * 0.97).toFixed(0)}`
+                          : this.props.currency === "zł"
+                          ? `zł ${(product.price * 4.54).toFixed(0)}`
+                          : this.props.currency === "₴"
+                          ? `₴ ${(product.price * 36.8).toFixed(0)}`
+                          : `$ ${product.price}`}
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ) : (
+                <li
+                  key={product.id}
+                  className="productList__item-missing"
+                  onClick={() => {
+                    this.props.onProductSelected(product.id);
+                  }}
+                >
+                  <img
+                    className="productList__item-img"
+                    src={product.mainImage.url}
+                    alt={product.title}
+                  />
+                  <div className="productList__item-descr">
+                    <div className="productList__item-title">
+                      {product.title}
+                    </div>
+                    <div className="productList__item-price">OUT OF STOCK</div>
+                  </div>
+                </li>
+              )
+            )}
+      </main>
+    );
+  }
 }
 
 export default ProductList;
